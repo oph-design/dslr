684df23a69b6b64cc941fb0d71b46bd48cc9de1b
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-10 02:00:16 +0200

    implement draw_histogram calc_bin and main function

--- histogram.py
+++ histogram.py
@@ -3,26 +3,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import math
+import libft as ft
 
-colors = ["red", "green", "blue", "yellow"]
+
+colors = ["red", "orange", "blue", "green"]
 
 
-def draw_hist(data: pd.DataFrame, feature: str):
-    schools = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
-    histdata = np.array([])
-    for i in range(len(schools)):
-        grades = data.loc[data["Hogwarts House"] == schools[i], feature]
-        histdata = np.concatenate((histdata, grades))
-    plt.hist(histdata, bins=10, density=True, stacked=True)
-    plt.show()
+def calc_bins(arr):
+    arr = arr[np.logical_not(np.isnan(arr))]
+    width = 3.49 * ft.std(arr) / ft.count(arr) ** (1.0 / 3)
+    return np.arange(ft.min(arr), ft.max(arr) + width, width)
+
+
+def draw_histogram(data: pd.DataFrame, feature: str, ax):
+    nbins = calc_bins(data[feature].to_numpy())
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    for i in range(len(houses)):
+        arr = data.loc[data["Hogwarts House"] == houses[i], feature]
+        arr = arr[np.logical_not(np.isnan(arr))]
+        ax.hist(
+            arr,
+            bins=nbins,
+            color=colors[i],
+            label=houses[i],
+            alpha=0.4,
+            edgecolor=colors[i],
+            linewidth=1.0,
+        )
+    ax.legend(loc="upper right")
+    ax.set_title(feature)
 
 
 def main():
     data = load_data(sys.argv[1])
     if data is None:
         exit(1)
-    print(data)
-    draw_hist(data, "Arithmancy")
+    features = list(data.columns[6:])
+    size = len(features)
+    columns = math.ceil(math.sqrt(size))
+    rows = math.ceil(size / columns)
+    fig = plt.figure(figsize=(6 * columns, 3 * rows))
+    for idx in range(size):
+        ax = fig.add_subplot(rows, columns, idx + 1)
+        draw_histogram(data, features[idx], ax)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
