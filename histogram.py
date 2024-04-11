4945a35904ad0faae080d8f4f5b48a083573a63c
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-12 01:20:31 +0200

    update histogram.py

--- histogram.py
+++ histogram.py
@@ -30,11 +30,9 @@ def draw_histogram(data: pd.DataFrame, feature: str, ax):
             linewidth=1.0,
         )
     ax.legend(loc="upper right")
-    ax.set_title(feature)
 
 
-def main():
-    data = ft.check_input(sys.argv, 1)
+def draw_all(data: pd.DataFrame):
     features = list(data.columns[6:])
     size = len(features)
     columns = math.ceil(math.sqrt(size))
@@ -43,6 +41,16 @@ def main():
     for idx in range(size):
         ax = fig.add_subplot(rows, columns, idx + 1)
         draw_histogram(data, features[idx], ax)
+        ax.set_title(features[idx])
+
+
+def main():
+    data = ft.check_input(sys.argv, 1)
+    if len(sys.argv) == 3:
+        draw_histogram(data, sys.argv[2], plt)
+        plt.title(sys.argv[2])
+    else:
+        draw_all(data)
     plt.tight_layout()
     plt.show()
 
