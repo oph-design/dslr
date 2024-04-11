7dca222195dc1d789a02601397302b3ebfd98ed9
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-11 03:48:42 +0200

    implement 1 time comparison for each feature

--- scatter_plot.py
+++ scatter_plot.py
@@ -3,35 +3,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-import math
 
-
+plt.rcParams.update({"font.size": 6})
 colors = ["red", "orange", "blue", "green"]
 
 
-def draw_scatterplot(data: pd.DataFrame, feature1: str, feature2: str, ax):
+def draw_scatterplot(data: pd.DataFrame, feat: str, comp: str, axes):
     houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
     for i in range(len(houses)):
-        y = data.loc[data["Hogwarts House"] == houses[i], feature2]
-        x = data.loc[data["Hogwarts House"] == houses[i], feature1]
-        ax.scatter(x, y, marker=".", color=colors[i], label=houses[i])
-    ax.set_title(f"{feature1}/{feature2}")
+        y = data.loc[data["Hogwarts House"] == houses[i], feat]
+        x = data.loc[data["Hogwarts House"] == houses[i], comp]
+        axes.scatter(x, y, marker=".", color=colors[i], label=houses[i])
+    axes.set_xlabel(comp)
+    axes.set_ylabel(feat)
 
 
 def main():
     data = load_data(sys.argv[1])
     if data is None:
         exit(1)
-    features = list(data.columns[6:])
-    size = len(features)
-    fig = plt.figure(figsize=(4 * size, 2 * size))
     n = 1
-    for i in range(size):
-        for j in range(i):
-            ax = fig.add_subplot(size, size, n)
-            draw_scatterplot(data, features[i], features[j], ax)
-            n += 1
-    plt.tight_layout()
+    features = list(data.columns[6:])
+    compare = features[1:]
+    size = len(features)
+    fig = plt.figure(figsize=(3 * size, 1 * size))
+    for feat in features:
+        for i, comp in enumerate(compare):
+            rowlen = len(compare)
+            axes = fig.add_subplot(size, size, n)
+            draw_scatterplot(data, feat, comp, axes)
+            n = n + 1 if i != rowlen - 1 else n + size - i
+        compare = compare[1:]
+    fig.tight_layout()
+    plt.subplots_adjust(left=0.1, wspace=0.5)
     plt.show()
 
 
