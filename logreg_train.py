a9312ffee0c444efae71227a4f17a7b5cbe4aca2
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-19 18:26:27 +0200

    implement strcuture for logreg_train

---
+++ logreg_train.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from libft import check_input
+import sys
+
+houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+
+def label_data(data: pd.DataFrame,label: str) -> pd.DataFrame:
+    data["Hogwarts House"] = (data["Hogwarts House"] == label).astype(int)
+    return data
+
+def get_features(data: pd.DataFrame):
+    return data.loc[
+        :,
+        [
+            "Hogwarts House",
+            "Astronomy",
+            "Herbology",
+            "Defense Against the Dark Arts",
+            "Ancient Runes",
+            "Charms",
+        ],
+    ]
+
+def perform_regression(data: pd.DataFrame) -> np.ndarray:
+    return []
+
+def main():
+    data = get_features(check_input(sys.argv, 0))
+    coefs = pd.DataFrame(columns=[
+        "Gryffindor",
+        "Slytherin",
+        "Ravenclaw",
+        "Hufflepuff",
+    ])
+    for house in houses:
+        coefs[house] = perform_regression(label_data(data, house))
+    coefs.to_csv("coefs.csv")
+    
+    
+
+
+if __name__ == "__main__":
+    main()
