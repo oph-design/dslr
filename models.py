368dd04e1fa6f99602db6d70b16a74d434381db6
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-26 18:39:41 +0200

    change models.py to include variable coef count

--- models.py
+++ models.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class GradientDescent:
 
-    def __init__(self, data: pd.DataFrame, feature: str):
+    def __init__(self, data: pd.DataFrame):
         self.c = 0.0
-        self.m = 0.0
         self.y = np.array(data.iloc[:, 0], dtype=np.float128)
-        self.x = np.array(data[feature], dtype=np.float128)
         self.n = len(self.y)
+        self.m = np.zeros(self.n)
+        self.x = np.array(data.iloc[:, 1:], dtype=np.float128)
         self.l = 0.01
         self.epochs = 1000
 
@@ -20,11 +20,12 @@ class GradientDescent:
 
     def _train(self):
         for i in range(self.epochs):
-            prediction = self._predict()
-            cost_c = (1 / self.n) * np.sum(prediction - self.y)
+            predict = self._predict()
+            cost_c = (1 / self.n) * np.sum(predict - self.y)
             self.c = self.c - self.l * cost_c
-            cost_m = (1 / self.n) * np.sum((prediction - self.y) * self.x)
-            self.m = self.m - self.l * cost_m
+            for j in range(self.n):
+                cost_m = (1 / self.n) * np.sum((predict - self.y) * self.x[j])
+                self.m[j] = self.m[j] - self.l * cost_m
 
     def _getCoefs(self):
-        return np.array([self.c, self.m])
+        return np.insert(self.m, 0, self.c)
