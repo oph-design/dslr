76376920e2eb7565a7b913f385a9c7e1a9f9dc66
oph-design <ole.paul.heinzelmann@gmail.com>
2024-04-28 17:08:16 +0200

    remove visualisation

--- logreg_train.py
+++ logreg_train.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from models import GradientDescent as GD
 from libft import check_input
@@ -35,26 +34,12 @@ def main():
             "Hufflepuff",
         ]
     )
-    i = 0
-    fig = plt.figure(figsize=(6 * 2, 3 * 2))
     for house, subjects in houses.items():
-        i += 1
         features = label_data(data.loc[:, subjects], house)
-        axes = fig.add_subplot(2, 2, i)
-        axes.scatter(
-            np.arange(0, len(features.iloc[:, 0])),
-            np.sort(features.iloc[:, 0]),
-            marker=".",
-        )
         model = GD(features)
         model._train()
-        coefs_h = model._getCoefs()
-        scores = coefs_h[0] + np.sum(coefs_h[1:] * features.iloc[:, 1:], axis=1)
-        probs = 1 / (1 + np.exp(scores * -1))
-        axes.plot(np.arange(0, len(features.iloc[:, 0])), np.sort(probs), color="g")
-        axes.set_title(house)
-        # coefs[house] = model._getCoefs()
-    # coefs.to_csv("coefs.csv", index=False)
+        coefs[house] = model._getCoefs()
+    coefs.to_csv("coefs.csv", index=False)
     plt.tight_layout()
     plt.show()
 
