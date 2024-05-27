f38684967f8a4c13619b8b3ce811a5be837c9272
oph-design <ole.paul.heinzelmann@gmail.com>
2024-05-27 04:31:37 +0200

    update requirements

--- test.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import accuracy_score
-import pandas as pd
-
-predicted_data = pd.read_csv("houses.csv")["Hogwarts House"].values
-correct_data = pd.read_csv("test.csv")["Hogwarts House"].values
-print(f"{accuracy_score(predicted_data, correct_data) * 100}%")
